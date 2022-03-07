a063e96837cfa2359f0618cb62dc4a3297369742
Pankaj Tiwari <patiwari@ciena.com>
2022-03-07 21:14:22 +0530

    Added basic ls command, metamodel and devices command

--- bpi-cli.py
+++ bpi-cli.py
@@ -91,6 +91,46 @@ class App:
             output_list.append((index,device_name,vendor,cateogry,latest,drniId,description))
         return output_list
 
+    def get_metamodels(self):
+        with self.driver.session() as session:
+            result = session.read_transaction(self._get_metamodels)
+            table = BeautifulTable()
+            table.columns.width = [5,40, 15, 15, 15]
+            table.columns.header=["#", "name","metamodel_id","latest","hypermodel_id"]
+            index = 0
+            for row in result:
+                index = index+1
+                device_name = row.get("name", "") 
+                vendor = row.get("metamodel_id","") 
+                cateogry = row.get("latest", "") 
+                latest = row.get("hypermodel_id","") 
+                
+                table.rows.append([index, device_name,vendor,cateogry,latest])
+            
+            table.columns.header.alignment= BeautifulTable.ALIGN_LEFT
+            table.columns.alignment = BeautifulTable.ALIGN_LEFT
+            print(table)
+
+    def get_folders(self, folder_name):
+        with self.driver.session() as session:
+            result = session.read_transaction(self._get_folder_list,folder_name)
+            table = BeautifulTable()
+            table.columns.width = [5,40, 15, 15, 15]
+            table.columns.header=["#", "name","metamodel_id","latest","hypermodel_id"]
+            index = 0
+            for row in result:
+                index = index+1
+                device_name = row.get("name", "") 
+                vendor = row.get("metamodel_id","") 
+                cateogry = row.get("latest", "") 
+                latest = row.get("hypermodel_id","") 
+                
+                table.rows.append([index, device_name,vendor,cateogry,latest])
+            
+            table.columns.header.alignment= BeautifulTable.ALIGN_LEFT
+            table.columns.alignment = BeautifulTable.ALIGN_LEFT
+            print(table)
+
     @staticmethod
     def _get_devices(tx):
         query = (
@@ -100,11 +140,63 @@ class App:
         result = tx.run(query)
         return [row["n"] for row in result]
 
+    @staticmethod
+    def _get_metamodels(tx):
+        query = (
+            "match (d:Metamodel) return distinct d.name as name,"
+            " d.metamodelId as metamodel_id, d.latest as latest,d.hypermodelId as hypermodel_id order by d.name"
+        )
+        result = tx.run(query)
+        return [row for row in result]
+    
+    @staticmethod
+    def _get_folder_list(tx,folder_name):
+        query = (
+            "match (d:{0}) return distinct d.name as name,"
+            " d.metamodelId as metamodel_id, d.latest as latest,d.hypermodelId as hypermodel_id order by d.name"
+        ).format(folder_name)
+
+        result = tx.run(query)
+        return [row for row in result]
+
+
+def cli_label(folder_stack):
+    return "/".join(folder_stack)
 
 if __name__ == "__main__":
     bolt_url = "bolt://localhost:7687"
     user = "neo4j"
     password = "drni"
     app = App(bolt_url, user, password)
-    app.get_devices()
+    folder_stack = []
+
+    while True:
+        command = input("BPI-CLI/{0}>>".format(cli_label(folder_stack)))
+        try:
+            if command == "devices":
+                app.get_devices()
+            elif command == "metamodels":
+                app.get_metamodels()
+            elif command.startswith("cd "):
+                if command == "cd ..":
+                    if len(folder_stack)!=0:
+                        folder_stack.pop()
+                else:
+                    folder = command.split(" ")[1]
+                    print("Folder = {0}".format(folder))
+                    folder_stack.append(folder)
+            elif command == "ls":
+                if len(folder_stack)>0:
+                    present_folder = folder_stack[-1]
+                    app.get_folders(present_folder)
+                else:
+                    app.get_metamodels()
+            elif command == "exit":
+                break
+            elif command == "q":
+                break
+        except Exception as e:
+            print("Malformed DB Query")
+            print(e)
+
     app.close()
